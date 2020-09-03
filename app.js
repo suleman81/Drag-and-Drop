3bcae0b5f6b4b05839dc9a3ae0c705d726438697
SulemanBajwa <58045793+suleman81@users.noreply.github.com>
2020-09-03 12:25:55 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+let pink = document.getElementById("pink");
+let green = document.getElementById("green");
+let red = document.getElementById("red");
+
+function Pink(){
+    setTimeout(function()
+    { 
+        green.style.backgroundColor = "#FF56A0";
+        
+    }, 800);
+    
+}
